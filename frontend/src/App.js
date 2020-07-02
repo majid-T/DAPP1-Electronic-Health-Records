e4c59619b095ae5e23e420dddf369f089f1d0611
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-07-02 21:34:59 +0430

    Medical Card skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,17 +8,33 @@ import DoctorB from "./components/DoctorB";
 import FamilyMember from "./components/FamilyMember";
 
 function App() {
+  const patientName = "Patient A";
+  const drA_Name = "Doctor A";
+  const drb_Name = "Doctor B";
+  const fmlyMemName = "Family Member for Patient A";
+
   return (
     <BrowserRouter>
       <Navigation />
       <div className="mainPanel">
         <Switch>
-          <Route path="/Patient" render={(props) => <Patient {...props} />} />
-          <Route path="/DoctorA" render={(props) => <DoctorA {...props} />} />
-          <Route path="/DoctorB" render={(props) => <DoctorB {...props} />} />
+          <Route
+            path="/Patient"
+            render={(props) => <Patient {...props} patientName={patientName} />}
+          />
+          <Route
+            path="/DoctorA"
+            render={(props) => <DoctorA {...props} drName={drA_Name} />}
+          />
+          <Route
+            path="/DoctorB"
+            render={(props) => <DoctorB {...props} drName={drb_Name} />}
+          />
           <Route
             path="/FamilyMember"
-            render={(props) => <FamilyMember {...props} />}
+            render={(props) => (
+              <FamilyMember {...props} fmlyMemberName={fmlyMemName} />
+            )}
           />
         </Switch>
       </div>
