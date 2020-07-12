1f4e3edd224c045eee2afc5f49293104fcd8fe98
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-07-13 03:14:53 +0430

    Added CORS headers

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,17 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/record", medicalRouter);
+
+//for CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
