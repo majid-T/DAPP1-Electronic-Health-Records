6729888c6e20ab07bd3692ec74b89b782bef4451
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-07-13 02:54:51 +0430

    pre commit on changes

--- backend/routes/medical.js
+++ backend/routes/medical.js
@@ -9,6 +9,83 @@ const adminId = "admin";
 // Create a new file system based wallet for managing identities.
 // Majid NOTE: Below path does not exist yet
 
+router.get("/register-user-get", async function (req, res, next) {
+  const { patientName } = req.query;
+
+  try {
+    const ccpPath = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "fabric-samples",
+      "test-network",
+      "organizations",
+      "peerOrganizations",
+      "org1.example.com",
+      "connection-org1.json"
+    );
+    console.log("ccpPath", ccpPath);
+    const walletPath = path.join(process.cwd(), "wallet");
+    const wallet = await Wallets.newFileSystemWallet(walletPath);
+    //const wallet = new FileSystemWallet(walletPath);
+
+    //cosnole the wallet path
+    console.log(`Wallet path: ${walletPath}`);
+    console.log("wallet", wallet);
+    // Check to see if we've already enrolled the user.
+    /*const identity = await wallet.get(adminId);
+  if (!identity) {
+    console.log(`ERROR: Couldnt find the identity ${adminId}`);
+  }*/
+    const userExists = await wallet.get(adminId);
+    console.log("userExists", userExists);
+    if (!userExists) {
+      console.log(`An identity for the user  does not exist in the wallet`);
+      res.json({
+        status: "failed",
+        message: `An identity for the user ${adminId} does not exist in the wallet`,
+      });
+      return;
+    }
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    console.log("gateway", gateway);
+    // use the identity of user1 from wallet to connect
+    await gateway.connect(ccpPath, {
+      wallet,
+      identity: adminId,
+      discovery: { enabled: true, asLocalhost: true },
+    });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork("mychannel");
+    console.log("network", network);
+    // Get the contract from the network.
+    const contract = network.getContract("medical");
+    console.log("contract", contract);
+
+    // Evaluate the specified transaction.
+
+    const result = await contract.submitTransaction(
+      "createPatientRecord",
+      patientName
+    );
+    console.log(
+      `Transaction has been evaluated, result is: ${result.toString()}`
+    );
+    await gateway.disconnect();
+    res.json({
+      status: "success",
+      message: `create patient successful`,
+    });
+  } catch (error) {
+    res.json({
+      status: "failed",
+      message: `Failed to create a user ${error}`,
+    });
+  }
+});
 /* post createPatiendRecord */
 router.post("/register-user", async function (req, res, next) {
   const { patientName } = req.body;
@@ -17,7 +94,13 @@ router.post("/register-user", async function (req, res, next) {
     const ccpPath = path.resolve(
       __dirname,
       "..",
-      "config",
+      "..",
+      "..",
+      "fabric-samples",
+      "test-network",
+      "organizations",
+      "peerOrganizations",
+      "org1.example.com",
       "connection-org1.json"
     );
     console.log("ccpPath", ccpPath);
--- devApi.js
+++ devApi.js
@@ -67,6 +67,9 @@ const getMedicalContract = async () => {
     __dirname,
     "..",
     "..",
+    "..",
+    "HLF_SETUP",
+    "fabric-samples",
     "test-network",
     "organizations",
     "peerOrganizations",
@@ -121,8 +124,43 @@ app.get("/register-user", async function (req, res, next) {
   const { patientName } = req.query;
 
   try {
-    //get contract instance
-    const contract = await getMedicalContract();
+    const ccpPath = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "HLF_SETUP",
+      "fabric-samples",
+      "test-network",
+      "organizations",
+      "peerOrganizations",
+      "org1.example.com",
+      "connection-org1.json"
+    );
+
+    const ccp = JSON.parse(fs.readFileSync(ccpPath, "utf8"));
+    // Create a new file system based wallet for managing identities.
+    const walletPath = path.join(process.cwd(), "wallet");
+    const wallet = await Wallets.newFileSystemWallet(walletPath);
+    console.log(`Wallet path: ${walletPath}`);
+
+    // Check to see if we've already enrolled the user.
+    const identity = await wallet.get(adminId);
+    if (!identity) {
+      console.log(`ERROR: Couldnt find the identity ${adminId}`);
+    }
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, {
+      wallet,
+      identity: "admin",
+      discovery: { enabled: true, asLocalhost: true },
+    });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork("mychannel");
+    // Get the contract from the network.
+    const contract = network.getContract("medical");
 
     // Evaluate the specified transaction.
     const result = await contract.submitTransaction(
@@ -132,6 +170,8 @@ app.get("/register-user", async function (req, res, next) {
     console.log(
       `Transaction has been evaluated, result is: ${result.toString()}`
     );
+    // Disconnect from the gateway.
+    await gateway.disconnect();
     res.json({
       status: "success",
       message: `create patient successful`,
